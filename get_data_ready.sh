771285b270a1cec8b11b46e3075d36bf366eab0d
OkusamaOwO <lanhuong2k4UwU@gmail.com>
2025-09-25 21:14:58 +0700

    data process final boss

---
+++ get_data_ready.sh
@@ -0,0 +1,4 @@
+#tar -xvzf ./mydataset/clear.tar.gz -C /workspace/mydataset/
+#mv /workspace/mydataset/content/mount/MyDrive/2025fog/clear /workspace/mydataset/
+#tar -xvzf ./mydataset/foggy_zurich_direct.tar.gz -C /workspace/mydataset/
+#mv /workspace/mydataset/content/mount/MyDrive/2025fog/foggy_zurich_direct /workspace/mydataset/
