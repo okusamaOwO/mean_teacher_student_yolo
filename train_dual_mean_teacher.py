6eedfa98352c9c12438f14febb146573fd6f97a1
okusamaOwO <lanhuong2k4uwu@gmail.com>
2025-09-30 11:57:23 +0000

    runable code for mini data

--- train_dual_mean_teacher.py
+++ train_dual_mean_teacher.py
@@ -97,7 +97,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
     with torch_distributed_zero_first(LOCAL_RANK):
         data_dict = data_dict or check_dataset(data)  # check if None
     train_path, val_path = data_dict['train'], data_dict['val']
-    unsupervised_data_path = "/workspace/mydataset/foggy_zurich_direct/train/images"
+    unsupervised_data_path = "/content/mean_teacher_student_yolo/mini/train/images"
     nc = 1 if single_cls else int(data_dict['nc'])  # number of classes
     names = {0: 'item'} if single_cls and len(data_dict['names']) != 1 else data_dict['names']  # class names
     # is_coco = isinstance(val_path, str) and val_path.endswith('coco/val2017.txt')  # COCO dataset
